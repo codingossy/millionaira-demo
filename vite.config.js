b2925e232503bb8ce1c7951ace53e8eb52d2473b
dev-phantom <godsfavour1975@gmail.com>
2023-03-05 01:27:09 +0100

    Feat: added PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import path from 'path'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+     })
+  ],
 })
